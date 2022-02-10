6c91887664b2c6a9d881d85eadbcfdad2db10d4c
Marwin_Rop <68537635+Marwin5411-Winner@users.noreply.github.com>
2022-02-11 00:06:20 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -26,9 +26,21 @@ router.get('/player/:player_uid/:player/:score',(req,res)=>{
   const player_uid = req.params.player_uid;
   const player = req.params.player;
   const score = req.params.score;
-  const sql = `INSERT INTO hello (player_uid,player_name,score) VALUES ('${player_uid}','${player}','${score}')`;
-  db.query(sql ,(err,result)=>{
+  const INSERT = `INSERT INTO hello (player_uid,player_name,score) VALUES ('${player_uid}','${player}','${score}')`;
+  const update = `UPDATE hello SET score = '${score}' WHERE player_uid = '${player_uid}'`;
+  const select = `SELECT * FROM hello WHERE player_uid = '${player_uid}'`;
+  db.query(select,(err,result)=>{
     if (err) throw err;
+    if (result.length == 0) {
+      db.query(INSERT,(err,result)=>{
+        if (err) throw err;
+      });
+    }
+    else {
+      db.query(update,(err,result)=>{
+        if (err) throw err;
+      });
+    }
   });
   console.log(player + " " + score + " " + player_uid + " ");
   res.send("Success");
